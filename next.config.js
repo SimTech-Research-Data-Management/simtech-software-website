e1dc503b589ce748db209360e5d64fc5c1c43414
Jan Range <30547301+JR-1991@users.noreply.github.com>
2023-11-08 17:53:43 +0100

    added env variable key

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        DATAVERSE_KEY: process.env.DATAVERSE_KEY,
+    },
+}
 
 module.exports = nextConfig
