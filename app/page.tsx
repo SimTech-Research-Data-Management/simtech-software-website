d3576df0323fde21046581b939c6f565b22e7563
Jan Range <range.jan@web.de>
2024-04-22 17:41:36 +0200

    formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Link from "next/link"
-import Card from "./card"
-import fetchDatasets from "./dvloader"
-import { Dataset, DatasetCollection } from "./types"
+import fetchDatasets from "./dvloader";
+import { DatasetCollection } from "./types";
 import Collection from "./collection";
 import SideBar from "./sidebar";
 
@@ -9,17 +7,14 @@ interface queryParams {
   key?: string;
 }
 
-export default async function Home(
-  {
-    params,
-    searchParams,
-  }: {
-    params: { slug: string };
-    searchParams: queryParams;
-  }
-) {
-
-  const collections = await fetchDatasets()
+export default async function Home({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: queryParams;
+}) {
+  const collections = await fetchDatasets();
 
   return (
     <div className="p-20">
@@ -32,21 +27,21 @@ export default async function Home(
           <SideBar collections={collections} />
         </div>
         <div className="col-span-9 mx-5 -translate-y-24">
-          {
-            collections.map((collection: DatasetCollection, index: number) => {
-              if (searchParams.key === collection.name || searchParams.key === undefined) {
-                return (
-                  <Collection
-                    collection={collection}
-                    key={"collection" + index.toString()}
-                  />
-                )
-              }
-            })
-          }
+          {collections.map((collection: DatasetCollection, index: number) => {
+            if (
+              searchParams.key === collection.name ||
+              searchParams.key === undefined
+            ) {
+              return (
+                <Collection
+                  collection={collection}
+                  key={"collection" + index.toString()}
+                />
+              );
+            }
+          })}
         </div>
       </div>
-
     </div>
-  )
+  );
 }
